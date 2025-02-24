8e1f777be6266639d03b63618f5141c5f410959f
agusaguerom <agusaguerom28@gmail.com>
2025-02-24 12:25:01 -0300

    .

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
   return (
     <>
     <div className="containerAbout">
-      <p>Me llamo Agustin Aguero, tengo 19 años y soy de Buenos Aires Argentina. <strong>Actualmente me encuentro cursando el tercer año de la carrera de Analista de Sistemas.</strong> Me encanta enfocarme en el desarrollo de software, ofreciendo una experiencia óptima y completa a los usuarios.
+      <p>Me llamo Agustin Aguero, tengo 19 años y soy de Buenos Aires Argentina. <strong>Actualmente me encuentro cursando el tercer año y ultimo de la carrera de Analista de Sistemas en Escuela Da Vinci.</strong> Me encanta enfocarme en el desarrollo de software, ofreciendo una experiencia óptima y completa a los usuarios.
 
       </p>
       <p><strong>Poseo conocimientos de diversas tecnologías y metodologías</strong> para realizar proyectos de forma eficiente. Durante mi formacion he aprendido y aplicado metodologías agiles como lo son: Scrum y Kanban. Ademas tengo habilidades en distintos lenguajes de programacion, manejo de base de datos y el uso de frameworks para el desarrollo de web y de software</p>
