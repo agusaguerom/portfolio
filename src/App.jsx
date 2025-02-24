870d00f7dea48dd56fd255405e8c0489782a9dfb
agusaguerom <agusaguerom28@gmail.com>
2025-02-24 12:57:06 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import './index.css'
+
 import ParticlesBackground from './components/Config/ParticlesBackground'
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home'
